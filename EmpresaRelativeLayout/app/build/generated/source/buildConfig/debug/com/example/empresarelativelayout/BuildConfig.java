60f1642d86827380ff7269e714932f0dd686d871
fabiodasilva500 <fabiodasilva500@gmail.com>
2021-10-26 10:53:56 -0300

    subida de codigo

---
+++ EmpresaRelativeLayout/app/build/generated/source/buildConfig/debug/com/example/empresarelativelayout/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.empresarelativelayout;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.empresarelativelayout";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
